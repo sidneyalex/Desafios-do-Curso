912fa88c2aa25097cd2dd39f54309d0c529b0a4c
sidneyalex <79478176+sidneyalex@users.noreply.github.com>
2021-02-23 17:01:05 -0300

    Update Desafio006.py

--- Desafio006.py
+++ Desafio006.py
@@ -1,1 +1,8 @@
-#Desafio 06
+#Desafio 06 (Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.)
+numero = int(input('Digite um numero inteiro: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** (1/2)
+print('O numero digitado foi ', numero)
+print('Seu dobro = {} \nSeu triplo = {}'.format(dobro, triplo))
+print('Sua Raiz quadrada é {:.2f}'.format(raiz))
