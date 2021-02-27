e69564fb3f70faef9f07fc2bb09ce79150120228
sidneyalex <79478176+sidneyalex@users.noreply.github.com>
2021-02-26 23:57:23 -0300

    Desafio010 feito
    Feito o desafio010 e melhorado, mas esta dando diferença no resultado

--- Desafio010.py
+++ Desafio010.py
@@ -1,1 +1,6 @@
 #Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+r = float(input('Quanto de dinheiro tem na sua carteira? R$'))
+d = (r / 5.60)
+i = (d * 112)
+print('Você pode comprar {:.2f} Dólares.'.format(d))
+print('Com esses {:.2f}U$ você pode comprar {:.2f} Ienes.'.format(d, i))
