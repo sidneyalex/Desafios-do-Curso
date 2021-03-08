12be77ac69d5fdd3135d50ebad85a3728388d819
sidneyalex <79478176+sidneyalex@users.noreply.github.com>
2021-03-07 23:21:37 -0300

    Incluindo strip

--- Desafio024.py
+++ Desafio024.py
@@ -1,4 +1,4 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
-city = input('Digite o nome da cidade: ')
+city = input('Digite o nome da cidade: ').strip()
 city = city.upper()
 print('A cidade começa com Santo no nome? {}'.format(city[:5] == 'SANTO'))
