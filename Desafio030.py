d2774dddd345ff5f8f2104e86f928b042861f6df
sidneyalex <79478176+sidneyalex@users.noreply.github.com>
2021-03-09 19:38:11 -0300

    PAR ou IMPAR

---
+++ Desafio030.py
@@ -0,0 +1,6 @@
+#crie um pgm q leia um num int e mostre na tela se ele é PAR ou IMPAR.
+num = int(input('Digite um numero: '))
+if num%2 == 0:
+    print('O numero digitado é PAR')
+else:
+    print('O numero digitado é IMPAR')
