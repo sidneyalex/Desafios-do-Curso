b84cfb1b2a08774c76f9f0496daae4c339c44117
sidneyalex <79478176+sidneyalex@users.noreply.github.com>
2021-03-21 22:42:48 -0300

    Loop para opção invalida

---
+++ Desafio057.py
@@ -0,0 +1,5 @@
+#Faça um pgm que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
+sexo = str(input('Informe seu sexo? [M]/[F]: ')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input('Valor invalido. Pr favor, informe seu sexo? [M]/[F]: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso'.format(sexo))
