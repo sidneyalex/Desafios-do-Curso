edd73409a8fdc3a6894a664e61899c48ec81d7c8
sidneyalex <79478176+sidneyalex@users.noreply.github.com>
2021-03-23 19:02:29 -0300

    Soluções com while e for

--- Desafio060.py
+++ Desafio060.py
@@ -1,14 +1,19 @@
 #Faça um pgm que leia um numero qualquer e mostre seu fatorial.Ex: 5! = 5x4x3x2x1 = 120
 n = int(input('Digite um número: '))
-fat = n
-acumulado = n
-total = fat
-while fat != 1:
-    print('{} '.format(fat), end='')
-    fat = fat - 1
-    acumulado = acumulado * fat
-    if fat != 1:
-        print('x ', end='')
-    else:
-        print('x 1 = ', end='')
-print(acumulado)
+fatorial = 1
+print('Calculando {}! = '.format(n), end='')
+#Solução com while
+'''contador = n
+while contador > 0:
+    print('{}'.format(contador), end='')
+    print(' x ' if contador > 1 else ' = ', end='')
+    fatorial *= contador
+    contador -= 1'''
+#Fim da solução com while
+#Solução com for
+for c in range(n, 0, -1):
+    print(c, end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    fatorial *= c
+#Fim da solução com for
+print(fatorial)
