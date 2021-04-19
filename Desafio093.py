eaa05d65413a8ef7ac61637d37db8038be5f15e4
sidneyalex <79478176+sidneyalex@users.noreply.github.com>
2021-04-19 17:28:19 -0300

    Cadastro de jogador de futebol

---
+++ Desafio093.py
@@ -0,0 +1,18 @@
+#Crie um pgm q gerencie o aproveitamento de um jogador de futebol. O pgm vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionario, incluindo o total de gols feitos durante o campeonato
+cad = {'Nome': str(input('Nome do jogador: '))}
+partidas = int(input(f'Quantas partidas {cad["Nome"]} jogou? '))
+gols = list()
+for g in range(0, partidas):
+    gols.append(int(input(f'Gols na partida {g + 1}: ')))
+cad['Gols'] = gols[:]
+cad['Total'] = sum(gols)
+print('-=' * 30)
+print(cad)
+print('-=' * 30)
+for k, v in cad.items():
+    print(f'O campo {k} tem valor {v}')
+print('-=' * 30)
+print(f'O jogador {cad["Nome"]} jogou {len(cad["Gols"])} partidas.')
+for i, g in enumerate:
+    print(f' => Partida {g+1} - {cad["Gols"][g]} gols.')
+print(f'Foi um total de {cad["Total"]} gols.')
